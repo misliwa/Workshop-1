375ec332fd7ac159d6081a9aba64ae0bb1b84c2e
misliwa <msliwa@visa.com>
2026-07-03 10:28:37 +0200

    added PathConstants

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -2,9 +2,19 @@ package pl.coderslab;
 
 import pl.coderslab.constants.StringConstants;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskManager {
+    private List<String> tasks = new ArrayList<>();
+
     static void main() {
+        loadSavedTasksData();
         showMenu();
+    }
+
+    private static void loadSavedTasksData() {
+
     }
 
     private static void showMenu() {
---
+++ src/main/java/pl/coderslab/constants/PathConstants.java
@@ -0,0 +1,8 @@
+package pl.coderslab.constants;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathConstants {
+    public static final Path savedTasksFile = Paths.get("src/main/resources/tasks.csv");
+}
