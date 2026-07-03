2fddfcf7086b80735b992a00e8c024d923b9a092
misliwa <msliwa@visa.com>
2026-07-03 10:20:03 +0200

    added showMenu() method

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,7 +1,17 @@
 package pl.coderslab;
 
+import pl.coderslab.constants.StringConstants;
+
 public class TaskManager {
     static void main() {
-        
+        showMenu();
+    }
+
+    private static void showMenu() {
+        System.out.println(ConsoleColors.BLUE + StringConstants.MENU_HEADLINE);
+        System.out.print(ConsoleColors.RESET);
+        for(String menuItem : StringConstants.MENU_ITEMS){
+            System.out.println(menuItem);
+        }
     }
 }
---
+++ src/main/java/pl/coderslab/constants/StringConstants.java
@@ -0,0 +1,12 @@
+package pl.coderslab.constants;
+
+public class StringConstants {
+    public static final String MENU_HEADLINE = "Please select an option:";
+
+    public static final String[] MENU_ITEMS = {
+            "add",
+            "remove",
+            "list",
+            "exit"
+    };
+}
